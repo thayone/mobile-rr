419a141c830c61f08a911dbb5c0906639fddb910
Kasper Grubbe <kawsper@gmail.com>
2018-02-03 19:08:50 +0000

    Pacify or remove most of the beeper code

--- src/mobile-rr.ino
+++ src/mobile-rr.ino
@@ -82,8 +82,6 @@ bool DEBUG                  = 1;
 bool SILENT                 = 0;
 int interval                = 30;                                               // 30 Minutes
 
-#define PIEZO_PIN       4
-
 // Maximum number of simultaneous clients connected (WebSocket)
 #define MAX_WS_CLIENT   3
 
@@ -266,117 +264,19 @@ String formatBytes ( size_t bytes )
 //***************************************************************************
 void beep ( int delayms )
 {
-    digitalWrite ( PIEZO_PIN, HIGH );                                       // Turn PIEZO on
-    delay ( delayms );                                                      // wait for a delayms ms
-    digitalWrite ( PIEZO_PIN, LOW );                                        // Turn PIEZO off
+    delay ( delayms );
 }
 
 void beepC ( int delayms )
 {
-    for ( int c = 0; c < 448; c++ )
-    {
-        digitalWrite ( PIEZO_PIN, HIGH );
-        delayMicroseconds ( delayms );
-        digitalWrite ( PIEZO_PIN, LOW );
-        delayMicroseconds ( delayms );
-    }
-
-    delay ( 100 );
-
-    for ( int c = 0; c < 224; c++ )
-    {
-        digitalWrite ( PIEZO_PIN, HIGH );
-        delayMicroseconds ( delayms - ( delayms * .1 ) );
-        digitalWrite ( PIEZO_PIN, LOW );
-        delayMicroseconds ( delayms - ( delayms * .1 ) );
-    }
+  delay ( delayms );
 }
 
 void beep_rr ()
 {
-    // We'll set up an array with the notes we want to play
-    // change these values to make different songs!
-
-    // Length must equal the total number of notes and spaces
-
-    const int songLength = 18;
-
-    // Notes is an array of text characters corresponding to the notes
-    // in your song. A space represents a rest (no tone)
-
-    char notes[] = "cdfda ag cdfdg gf "; // a space represents a rest
-
-    // Beats is an array values for each note and rest.
-    // A "1" represents a quarter-note, 2 a half-note, etc.
-    // Don't forget that the rests (spaces) need a length as well.
-
-    int beats[] = {1, 1, 1, 1, 1, 1, 4, 4, 2, 1, 1, 1, 1, 1, 1, 4, 4, 2};
-
-    // The tempo is how fast to play the song.
-    // To make the song play faster, decrease this value.
-
-    int tempo = 125;
-
-    //dbg_printf("Tempo %d\n", tempo);
-
-    int i, duration;
-
-    digitalWrite ( LED_BUILTIN, LOW );
-
-    for ( i = 0; i < songLength; i++ ) // step through the song arrays
-    {
-        duration = beats[i] * tempo; // length of note/rest in ms
-
-        if ( notes[i] == ' ' ) // is this a rest?
-        {
-            delay ( duration ); // then pause for a moment
-        }
-        else                  // otherwise, play the note
-        {
-            analogWriteFreq ( frequency ( notes[i] ) );
-            analogWrite ( PIEZO_PIN, 800 );
-            delay ( duration );
-            analogWrite ( PIEZO_PIN, 0 );
-        }
-
-        delay ( tempo / 10 ); // brief pause between notes
-    }
-
-    digitalWrite ( LED_BUILTIN, HIGH );
-}
-
-int frequency ( char note )
-{
-    // This function takes a note character (a-g), and returns the
-    // corresponding frequency in Hz for the tone() function.
-
-    int i;
-    const int numNotes = 8; // number of notes we're storing
-
-    // The following arrays hold the note characters and their
-    // corresponding frequencies. The last "C" note is uppercase
-    // to separate it from the first lowercase "c". If you want to
-    // add more notes, you'll need to use unique characters.
-
-    // For the "char" (character) type, we put single characters
-    // in single quotes.
-
-    char names[] = { 'c', 'd', 'e', 'f', 'g', 'a', 'b', 'C' };
-    int frequencies[] = {262, 294, 330, 349, 392, 440, 494, 523};
-
-    // Now we'll search through the letters in the array, and if
-    // we find it, we'll return the frequency for that note.
-
-    for ( i = 0; i < numNotes; i++ ) // Step through the notes
-    {
-        if ( names[i] == note ) // Is this the one?
-        {
-            return ( frequencies[i] ); // Yes! Return the frequency
-        }
-    }
-
-    return ( 0 ); // We looked through everything and didn't find it,
-    // but we still need to return a value, so return 0.
+  digitalWrite ( LED_BUILTIN, LOW );
+  delay (100);
+  digitalWrite ( LED_BUILTIN, HIGH );
 }
 
 //***************************************************************************
@@ -407,9 +307,6 @@ void setup ( void )
     // Load EEPROM Settings
     setupEEPROM();
 
-    pinMode ( PIEZO_PIN, OUTPUT );                                          // initialize PIEZO PIN as output
-
-    if ( !SILENT ) beep_rr();
 
     // Setup Access Point
     wifi_set_phy_mode ( PHY_MODE_11B );
